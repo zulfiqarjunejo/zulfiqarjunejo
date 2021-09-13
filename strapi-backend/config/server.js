9d37bdfabf70695e7d215389dc2cc7effc08c3ab
Zulfiqar Ahmed <ahmed@cargoboard.com>
2021-09-13 09:36:41 +0200

    feat: moved strapi-backend to another repo

--- strapi-backend/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'a4160ed9e628601c5b2bc323569027c1'),
-    },
-  },
-});
--- strapi-backend/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '95fcbd2d-d60e-4a47-a0a0-fbaaadf46358'
-};
