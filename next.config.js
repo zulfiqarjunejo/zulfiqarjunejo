0e6a07294a55dae85ca25f46e4bd4b7619f6575c
Zulfiqar Ahmed <zulfiqar.ahmed@e-go-mobile.com>
2022-11-28 18:42:29 +0100

    fix: fix the strapi url

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    domains: [
+      "zulfiqarjunejo-strapi-backend.herokuapp.com",
+      "res.cloudinary.com",
+    ],
+  },
+  env: {
+    STRAPI_API_URL: "https://zulfiqarjunejo-strapi-backend.herokuapp.com",
+  },
 }
 
 module.exports = nextConfig
